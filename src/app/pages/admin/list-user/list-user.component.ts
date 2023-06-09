a997150603db08593547a77bd52f589d91fc7f41
Hoangldph21518 <Hoangldph21518@fpt.edu.vn>
2023-06-10 02:18:48 +0700

    feat:  fe-call-api-list-movies refs: WA-67

--- src/app/pages/admin/list-user/list-user.component.ts
+++ src/app/pages/admin/list-user/list-user.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-list-user',
   templateUrl: './list-user.component.html',
   styleUrls: ['./list-user.component.css']
 })
-export class ListUserComponent {
+export class ListUserComponent implements OnInit {
+  
+  users : User[] = [];
+  constructor (private MovieService: UserService){}
+  ngOnInit(): void {
+    this.MovieService.getUser().subscribe(data => {
+      this.users = data;
+      console.log(this.users);
+    })
+  }
 
 }
