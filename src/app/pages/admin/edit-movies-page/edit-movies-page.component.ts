d6fb6edb5bd616c1dd8243e43939334318872755
Hoangldph21518 <Hoangldph21518@fpt.edu.vn>
2023-06-12 16:24:04 +0700

    feat: fe-admin-update-movies refs: WA-75

--- src/app/pages/admin/edit-movies-page/edit-movies-page.component.ts
+++ src/app/pages/admin/edit-movies-page/edit-movies-page.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {FormBuilder ,Validators} from '@angular/forms';
+import { IMovie } from 'src/app/interfaces/movie';
+import { MoviesService } from 'src/app/services/movies.service';
+import {  ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -7,6 +12,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-movies-page.component.css']
 })
 export class EditMoviesPageComponent {
+  movies!: IMovie;
+  formMovie = this.formBuilder.group({
+    name: [''],
+    img: [''],
+    diem: [''],
+    nam: [''],
+    video: [''],
+    desc: [''],
+  })
+  constructor(
+    private formBuilder: FormBuilder,
+    private moviesService: MoviesService,
+    private route: ActivatedRoute,
+    private router:Router){
+      this.route.paramMap.subscribe(param => {
+        const id = String(param.get('id'));
+        this.moviesService.getProductById(id).subscribe(movies => {
+          this.movies = movies;
+          this.formMovie.patchValue({
+            name: movies.name,
+            img: movies.img,
+            diem: movies.diem,
+            nam: movies.nam,
+            video: movies.video,
+            desc: movies.desc,
+          }) 
+        }, error => console.log(error.message))
+      })
+    }
+  
+    onHanldeEditMovie(){
+      if (this.formMovie.valid) {
+        const movies: IMovie = {
+          _id: this.movies._id,
+          name: this.formMovie.value.name || "",
+          img: this.formMovie.value.img ||"",
+          diem: this.formMovie.value.diem || '',
+          nam: this.formMovie.value.nam || '',
+          video: this.formMovie.value.video ||'',
+          desc: this.formMovie.value.desc ||'',
+        }
+        this.moviesService.updateMovies(movies).subscribe(data => {
+          Swal.fire(' Thành công ', ' Update Movies thành công !', 'success');
+          this.router.navigate(['/admin/list-movies'])
+        })
+      }
+  
+    }
   
 
 }
--- src/app/pages/admin/list-user/list-user.component.ts
+++ src/app/pages/admin/list-user/list-user.component.ts
@@ -17,5 +17,5 @@ export class ListUserComponent implements OnInit {
       console.log(this.users);
     })
   }
-
+  
 }
