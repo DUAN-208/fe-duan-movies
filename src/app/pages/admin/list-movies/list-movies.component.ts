273869a152e8715fcfbf6956dc08073c5ab61923
Hoangldph21518 <Hoangldph21518@fpt.edu.vn>
2023-06-13 09:47:35 +0700

    feat/loi

--- src/app/pages/admin/list-movies/list-movies.component.ts
+++ src/app/pages/admin/list-movies/list-movies.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { IMovie } from 'src/app/interfaces/movie';
-import { MovieService } from 'src/app/services/services.service';
 import Swal from 'sweetalert2';
 import { MoviesService } from 'src/app/services/movies.service';
 @Component({
